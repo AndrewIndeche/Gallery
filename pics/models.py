6ea71e0ab91177fb7f93103bcbd7b0c88840437c
AndrewIndeche <indecheandrew@gmail.com>
2021-10-07 21:00:23 +0300

    update model class methods

--- pics/models.py
+++ pics/models.py
@@ -24,7 +24,14 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
-        
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
     def save_location(self):
         self.save()
 
@@ -43,6 +50,46 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
+    @classmethod
+    def get_all(cls):
+        '''
+        method to retrieve all images
+        '''
+        pics = Images.objects.all()
+        return pics
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        '''
+        method to retrieve images by unique id
+        '''
+        retrieved = Images.objects.get(id = id)
+        return retrieved
+
+    @classmethod
+    def search_image(cls, cat):
+        '''
+        method to search images by category
+        '''
+        retrieved = cls.objects.filter(category__name__contains=cat) #images assoc w/ this cat
+        return retrieved #list of instances
+
+
+    def save_image(self):
+        self.save()
 
     def delete_Image(self):
         self.delete()
+    def update_image(self, new_url):
+        '''
+        method to update an image's link
+        '''
+        try:
+            self.image_link = new_url
+            self.save()
+            return self
+        except self.DoesNotExist:
+            print('Image you specified does not exist')
