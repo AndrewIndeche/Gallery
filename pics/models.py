def2e18b80c91919cc8db5a0721db2a7dcebacd2
AndrewIndeche <indecheandrew@gmail.com>
2021-10-07 17:23:58 +0300

    initial test

--- pics/models.py
+++ pics/models.py
@@ -15,7 +15,7 @@ class Location(models.Model):
     model for location of Pics
     '''
     name = models.CharField(max_length=60)
-    
+
     def __str__(self):
         return self.name
 
@@ -26,6 +26,11 @@ class Image(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField()
     author = models.CharField(max_length=40, default='admin')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
+
+    def delete_Image(self):
+        self.delete()
--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Category,Location,Image
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.category= Category(name='home')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
