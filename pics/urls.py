1366040f14a3be030c8a41cb8831a2af2a19590f
AndrewIndeche <indecheandrew@gmail.com>
2021-10-07 20:12:44 +0300

    add routes on view page

--- pics/urls.py
+++ pics/urls.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.index,name = 'index'),
 ]
--- pics/views.py
+++ pics/views.py
@@ -3,5 +3,25 @@ from django.shortcuts import render
 from . models import Image, Location
 
 # Create your views here.
-def welcome(request):
+def index(request):
     return render(request, 'index.html')
+
+def gallery(request):
+    pictures = Images.get_all()
+    locations = Locations.get_all()
+    return render(request, 'gallery.html', {'pictures': pictures, 'locations':locations})
+
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        res = Images.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message':message, 'results':res})
+    else:
+        message = 'You have not searched any term'
+        return render(request, 'search.html', {'message':message})
+
+def location(request,locale):
+    images = Images.filter_by_location(locale)
+    return render(request, 'location.html', {'results':images})
