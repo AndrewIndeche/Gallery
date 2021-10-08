b685191bc913a3b85479d29b6e3ff5e5022963dc
AndrewIndeche <indecheandrew@gmail.com>
2021-10-09 00:41:27 +0300

    add style for css

--- picapp/settings.py
+++ picapp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'picapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = ( os.path.join('static'), )
