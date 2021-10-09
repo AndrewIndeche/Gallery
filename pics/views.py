b460e2442142071cb01540080e2e59d9277f8c78
AndrewIndeche <indecheandrew@gmail.com>
2021-10-09 15:20:45 +0300

    add styling to homepage

--- pics/views.py
+++ pics/views.py
@@ -7,14 +7,14 @@ def index(request):
     return render(request, 'index.html')
 
 def gallery(request):
-    pictures = Images.get_all()
-    locations = Locations.get_all()
+    pictures = Image.get_all()
+    locations = Location.objects.all()
     return render(request, 'gallery.html', {'pictures': pictures, 'locations':locations})
 
 def search(request):
     if 'category' in request.GET and request.GET['category']:
         search_term = request.GET.get('category')
-        res = Images.search_image(search_term)
+        res = Image.search_image(search_term)
         message = f'{search_term}'
 
         return render(request, 'search.html', {'message':message, 'results':res})
@@ -23,5 +23,5 @@ def search(request):
         return render(request, 'search.html', {'message':message})
 
 def location(request,locale):
-    images = Images.filter_by_location(locale)
+    images = Image.filter_by_location(locale)
     return render(request, 'location.html', {'results':images})
