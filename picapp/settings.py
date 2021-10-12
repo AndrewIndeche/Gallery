6400d80b7b00cc1b52ff92da837ea3233bf3a9e9
AndrewIndeche <86969595+AndrewIndeche@users.noreply.github.com>
2021-10-12 12:17:33 +0300

    Update settings.py

--- picapp/settings.py
+++ picapp/settings.py
@@ -24,10 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-<<<<<<< HEAD
-=======
 
->>>>>>> a4c114de14f7a4a00d05872e8726b36f4667dbb2
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
