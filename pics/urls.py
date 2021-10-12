d02ef2aa5dda8e3098a40feb1ae0a5eded30e9bf
AndrewIndeche <86969595+AndrewIndeche@users.noreply.github.com>
2021-10-12 23:33:06 +0300

    Update urls.py

--- pics/urls.py
+++ pics/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url(r'^$',views.index,name = 'index'),
-    url(r'^gallery/', views.gallery, name='gallery'),
-    url(r'^search/', views.search, name='search'),
-    url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
+    url('^$',views.index,name = 'index'),
+    url('^gallery/', views.gallery, name='gallery'),
+    url('^search/', views.search, name='search'),
+    url('^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
